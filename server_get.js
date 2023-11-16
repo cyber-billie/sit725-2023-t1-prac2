4bd923eaf00753ca4b920e29dc3c2dad0e459a69
cyber-billie <s222137149@deakin.edu.au>
2023-11-16 22:22:26 +1100

    commit

--- server_get.js
+++ server_get.js
@@ -15,6 +15,7 @@ const divideTwoNumber = (n1, n2) => {
 }
 
 
+/* 
 //Addition
 app.get("/add", (req, res) => {
     const n1 = parseInt(req.query.n1);
@@ -45,7 +46,26 @@ app.get("/divide", (req, res) => {
     const n2 = parseInt(req.query.n2);
     const result = divideTwoNumber(n1, n2).toFixed(2);
     res.json({statuscocde: 200, data: result });
+}) */
+
+
+//Calculations
+app.get("/calculate", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const add = addTwoNumber(n1, n2);
+    const sub = subtractTwoNumber(n1, n2);
+    const multiply = multiplyTwoNumber(n1, n2);
+    const divide = divideTwoNumber(n1, n2).toFixed(2);
+    res.json({
+        statuscocde: 200,
+        addition: add, 
+        subtraction: sub, 
+        multiplication: multiply, 
+        division: divide 
+    });
 })
+
 
 //console.log(addTwoNumber(19, 12));
 const port = 3040;
