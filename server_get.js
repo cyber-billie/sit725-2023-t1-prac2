ea54d55101d574a4d8e959b96a908649fa5f24dd
cyber-billie <s222137149@deakin.edu.au>
2023-11-16 21:49:47 +1100

    W2-Task2

---
+++ server_get.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const addTwoNumber = (n1, n2) => {
+    return n1 + n2;
+}
+const subtractTwoNumber = (n1, n2) => {
+    return n1 - n2;
+}
+const multiplyTwoNumber = (n1, n2) => {
+    return n1 * n2;
+}
+const divideTwoNumber = (n1, n2) => {
+    return n1 / n2;
+}
+
+
+//Addition
+app.get("/add", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = addTwoNumber(n1, n2);
+    res.json({ statuscocde: 200, data: result });
+});
+
+//Subtraction
+app.get("/subtract", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = subtractTwoNumber(n1, n2);
+    res.json({statuscocde: 200, data: result });
+})
+
+//Multiply
+app.get("/multiply", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = multiplyTwoNumber(n1, n2);
+    res.json({statuscocde: 200, data: result });
+})
+
+//Division
+app.get("/divide", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+    const result = divideTwoNumber(n1, n2).toFixed(2);
+    res.json({statuscocde: 200, data: result });
+})
+
+//console.log(addTwoNumber(19, 12));
+const port = 3040;
+
+app.listen(port, () => {
+    console.log("hello i'm listening to port " + port);
+})
